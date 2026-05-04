361712ca2edbec7f9140334509036416019131ff
tharun3421 <20bd1a0596@gmail.com>
2026-05-04 16:43:19 +0530

    changes in viteconfig

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://livedigit-ems-server.vercel.app',
+        changeOrigin: true,
+        secure: true,
+      }
+    }
+  }
 })

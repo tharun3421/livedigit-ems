11906ed5cc394096504e36cd77fffe20dede1ad2
tharun3421 <20bd1a0596@gmail.com>
2026-05-21 11:15:14 +0530

    changes in viteconfig

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,17 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        }
+      }
+    }
+  },
   server: {
-    hmr: {
-      hmr: false,
-    },
+    hmr: false,
     proxy: {
       '/api': {
         target: 'https://livedigit-ems-server.vercel.app',
